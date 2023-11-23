092c87549e2595cf39142ba13e83b3c76ed41b1a
Stefan Wagner <stefan.wagner@fh-ooe.at>
2023-11-23 16:08:01 +0100

    Update console output - again (corrected)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Git is cool. Really?");
+Console.WriteLine("Hello, World! Git is cool. Really? YESSSSS");
